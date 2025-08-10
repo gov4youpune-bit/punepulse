8d03fb2ef55cf6eee8d5f4404b0ed37d53fadf03
akshay3thakur <102411963+akshay3thakur@users.noreply.github.com>
2025-08-11 02:08:20 +0530

    final push 22

--- app/api/complaints/by-id/[id]/route.ts
+++ app/api/complaints/by-id/[id]/route.ts
@@ -1,5 +1,5 @@
 export const dynamic = "force-dynamic";
-
+export const revalidate = 0;
 
 import { NextResponse } from 'next/server';
 import { supabaseAdmin } from '@/lib/supabaseAdmin';
--- app/api/complaints/route.ts
+++ app/api/complaints/route.ts
@@ -1,3 +1,6 @@
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 // app/api/complaints/route.ts
 import { NextResponse } from 'next/server';
 import { supabaseAdmin } from '@/lib/supabaseAdmin';
